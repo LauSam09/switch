be0bfb3495188eee2b6fac9e03aaed5e91c8ede3
laurence ede <laurence.ede@gmail.com>
2020-01-31 07:53:33 +0000

    Added conditional filtering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App () {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!
+          Edit <code>src/App.js</code> and save to reload! - v3
         </p>
         <a
           className="App-link"
@@ -20,7 +20,7 @@ function App () {
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
