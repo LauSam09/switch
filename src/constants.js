a1e499f9ee17e9ee61c8d26ce0b666a4be13c65d
laurence ede <laurence.ede@gmail.com>
2020-02-24 20:49:17 +0000

    Added home category and made colours darker

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,15 @@
 export const REMOTE_DATABASE = process.env.REACT_APP_DATABASE_URL
 
 export const CATEGORIES = [
-  { name: 'other', color: '#ECECEC', fontColour: '#FFFFFF' },
-  { name: 'fresh', color: '#B4FEDC', fontColour: '#FFFFFF' },
-  { name: 'dairy', color: '#F0EAB3', fontColour: '#FFFFFF' },
+  { name: 'other', color: '#B7B7B7', fontColour: '#FFFFFF' },
+  { name: 'fresh', color: '#95D5B7', fontColour: '#FFFFFF' },
+  { name: 'dairy', color: '#E8DE86', fontColour: '#FFFFFF' },
   { name: 'cupboard', color: '#D0CEBA', fontColour: '#FFFFFF' },
   { name: 'drinks', color: '#A4C7DB', fontColour: '#FFFFFF' },
   { name: 'frozen', color: '#1B6DE3', fontColour: '#FFFFFF' },
   { name: 'meat', color: '#FFB0B0', fontColour: '#FFFFFF' },
-  { name: 'fruit and vegetables', color: '#8EC48C', fontColour: '#FFFFFF' }
+  { name: 'fruit and vegetables', color: '#74AC72', fontColour: '#FFFFFF' },
+  { name: 'home', color: '#9B8080', fontColour: '#FFFFFF' }
 ]
 
 export const DAYS = ['saturday', 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday']
